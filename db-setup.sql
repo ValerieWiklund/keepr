26dffa0e0a69db7662d1d00ce16fb6ecf70e6822
Valerie Wiklund <vkwiklund@gmail.com>
2019-10-18 13:52:41 -0600

    start-up. Created tables.

--- db-setup.sql
+++ db-setup.sql
@@ -1,69 +1,105 @@
--- CREATE TABLE users (
---     id VARCHAR(255) NOT NULL,
---     username VARCHAR(255) NOT NULL,
---     email VARCHAR(255) NOT NULL,
---     hash VARCHAR(255) NOT NULL,
---     PRIMARY KEY (id),
---     UNIQUE KEY email (email)
+-- CREATE TABLE users
+-- (
+--   id VARCHAR(255) NOT NULL,
+--   username VARCHAR(255) NOT NULL,
+--   email VARCHAR(255) NOT NULL,
+--   hash VARCHAR(255) NOT NULL,
+--   PRIMARY KEY (id),
+--   UNIQUE
+--   KEY email
+--   (email)
 -- );
 
--- CREATE TABLE vaults (
---     id int NOT NULL AUTO_INCREMENT,
---     name VARCHAR(255) NOT NULL,
---     description VARCHAR(255) NOT NULL,
---     userId VARCHAR(255),
---     INDEX userId (userId),
---     FOREIGN KEY (userId)
---         REFERENCES users(id)
---         ON DELETE CASCADE,  
+-- CREATE TABLE vaults
+-- (
+--   id int NOT NULL
+--   AUTO_INCREMENT,
+--     name VARCHAR
+--   (255) NOT NULL,
+--     description VARCHAR
+--   (255) NOT NULL,
+--     userId VARCHAR
+--   (255),
+--     INDEX userId
+--   (userId),
+--     FOREIGN KEY
+--   (userId)
+--         REFERENCES users
+--   (id)
+--         ON
+--   DELETE CASCADE,  
 --     PRIMARY KEY (id)
--- );
+--   );
 
--- CREATE TABLE keeps (
---     id int NOT NULL AUTO_INCREMENT,
---     name VARCHAR(255) NOT NULL,
---     description VARCHAR(255) NOT NULL,
---     userId VARCHAR(255),
---     img VARCHAR(255),
+--   CREATE TABLE keeps
+--   (
+--     id int NOT NULL
+--     AUTO_INCREMENT,
+--     name VARCHAR
+--     (255) NOT NULL,
+--     description VARCHAR
+--     (255) NOT NULL,
+--     userId VARCHAR
+--     (255),
+--     img VARCHAR
+--     (255),
 --     isPrivate TINYINT,
 --     views INT DEFAULT 0,
 --     shares INT DEFAULT 0,
 --     keeps INT DEFAULT 0,
---     INDEX userId (userId),
---     FOREIGN KEY (userId)
---         REFERENCES users(id)
---         ON DELETE CASCADE,  
+--     INDEX userId
+--     (userId),
+--     FOREIGN KEY
+--     (userId)
+--         REFERENCES users
+--     (id)
+--         ON
+--     DELETE CASCADE,  
 --     PRIMARY KEY (id)
--- );
+--     );
 
--- CREATE TABLE vaultkeeps (
---     id int NOT NULL AUTO_INCREMENT,
+--     CREATE TABLE vaultkeeps
+--     (
+--       id int NOT NULL
+--       AUTO_INCREMENT,
 --     vaultId int NOT NULL,
 --     keepId int NOT NULL,
---     userId VARCHAR(255) NOT NULL,
+--     userId VARCHAR
+--       (255) NOT NULL,
 
---     PRIMARY KEY (id),
---     INDEX (vaultId, keepId),
---     INDEX (userId),
+--     PRIMARY KEY
+--       (id),
+--     INDEX
+--       (vaultId, keepId),
+--     INDEX
+--       (userId),
 
---     FOREIGN KEY (userId)
---         REFERENCES users(id)
---         ON DELETE CASCADE,
+--     FOREIGN KEY
+--       (userId)
+--         REFERENCES users
+--       (id)
+--         ON
+--       DELETE CASCADE,
 
 --     FOREIGN KEY (vaultId)
---         REFERENCES vaults(id)
---         ON DELETE CASCADE,
+--       REFERENCES vaults
+--       (id)
+--         ON
+--       DELETE CASCADE,
 
 --     FOREIGN KEY (keepId)
---         REFERENCES keeps(id)
---         ON DELETE CASCADE
+--       REFERENCES keeps
+--       (id)
+--         ON
+--       DELETE CASCADE
 -- )
 
 
--- -- USE THIS LINE FOR GET KEEPS BY VAULTID
--- SELECT * FROM vaultkeeps vk
--- INNER JOIN keeps k ON k.id = vk.keepId 
--- WHERE (vaultId = @vaultId AND vk.userId = @userId) 
+--       -- USE THIS LINE FOR GET KEEPS BY VAULTID
+--       SELECT *
+--       FROM vaultkeeps vk
+--         INNER JOIN keeps k ON k.id = vk.keepId
+--       WHERE (vaultId = @vaultId AND vk.userId = @userId) 
 
 
 
@@ -72,3 +108,4 @@
 -- DROP TABLE IF EXISTS vaults;
 -- DROP TABLE IF EXISTS keeps;
 -- DROP TABLE IF EXISTS users;
+=
